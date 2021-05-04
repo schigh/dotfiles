373c10a89216c7bd065147469b7c4352767734f6
steve high <steve@thentwrk.com>
2021-05-04 11:16:17 -0400

    removed unused alias

--- alias.zsh
+++ alias.zsh
@@ -22,5 +22,3 @@ alias pubkey="cat ${HOME}/.ssh/${PUB_KEY_NAME}"
 
 # convert mov file to mp4 file (REQUIRES FFMPEG)
 alias mov2mp4="ffmpeg -i \"${1}.mov\" -vcodec h264 -acodec mp2 \"${1}.mp4\""
-
-alias codemaat='java -jar /Users/steve/projects/code-maat/target/code-maat-1.1-SNAPSHOT-standalone.jar -c git2'
