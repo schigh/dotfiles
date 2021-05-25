690e4d25ecc6450c00d0c79e7025993f725abc94
steve high <steve@thentwrk.com>
2021-05-25 11:53:55 -0400

    unsaved file

--- git.zsh
+++ git.zsh
@@ -5,7 +5,4 @@
 alias wat='git status'
 
 # remove any stale branches locally (only works with master)
-alias gitclean="git branch --merged master | grep -v '^[ *]*master$' | xargs git branch -d"
-
-# remove any stale branches locally
-alias gcmain="git branch --merged main | grep -v '^[ *]*main$' | xargs git branch -d"
+alias gitclean="git branch --merged ${1} | grep -v '^[ *]*${1}$' | xargs git branch -d"
